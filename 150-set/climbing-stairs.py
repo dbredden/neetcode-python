76c509acb84a65d88f63633717b6a038106babf6
Danny Redden <dredden@allegion.com>
2025-03-05 12:33:15 -0500

    added folders + climbing stairs solution

---
+++ 150-set/climbing-stairs.py
@@ -0,0 +1,19 @@
+# climbing a staircase and it takes n steps to reach the top
+# each time you climb 1 or 2 steps. In how many distinct ways can you reach the top?
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
+
+        for i in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+        
+        print(one)
+        return one
+    
+
+
+""" solution = Solution()
+result = solution.climbStairs(5)  # This will print the result for n = 5 """
