a64e21714127bed980addd6fb207bcaa9e42bd2b
Danny Redden <dredden@allegion.com>
2025-03-12 16:24:46 -0400

    reorganization + insertion sort fun

---
+++ lessons/sorting-algos/insertion-sort.py
@@ -0,0 +1,19 @@
+def InsertionSort(arr):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+        j = i - 1
+        while j >= 0 and arr[j + 1] < arr[j]:
+            # arr[j] and arr[j + 1] are out of order, so swap them
+            temp = arr[j + 1]
+            arr[j + 1] = arr[j]
+            arr[j] = temp
+            j -= 1
+    return arr
+
+words = ["apple", "grape", "banana", "clemintime"]
+sorted_words = InsertionSort(words)
+print(sorted_words)
+
+someNums = [1, 2, 3, 44, 22, 15, 9]
+sorted_nums = InsertionSort(someNums)
+print(someNums)
