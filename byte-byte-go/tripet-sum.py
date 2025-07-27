d3895612de5da9388eb0f4d532cd52026871e8e3
Danny Redden <dannyredden18@gmail.com>
2025-07-27 15:36:36 -0400

    syntax fixes + comments

--- byte-byte-go/tripet-sum.py
+++ byte-byte-go/tripet-sum.py
@@ -5,52 +5,35 @@ def triplet_sum(nums: List[int]) -> List[List[int]]:
     nums.sort()
 
     for i in range(len(nums)):
-        # triplets consiting of only positive number will never be sum to 0
+        # the 'a' value will never be larger than 0 becuase that implies all proceeding values are positive
         if nums[i] > 0:
             break
-        # to avoid duplicate triplets, skip 'a' if its the same as the previous number
+        # here we want to continue on if the next value is the same as the previous
         if i > 0 and nums[i] == nums[i - 1]:
             continue
-        # find all pairs that sum to a target of '-a' (-nums[i])
-        pairs = pair_sum_sorted_all_pairs(nums, i + 1, -nums[1])
+        # pass in the array, the start value, and the target
+        # the target for this is the -(nums - 1)
+        pairs = pair_sum_sorted_all_pairs(nums, i + 1, -nums[i])
         for pair in pairs:
             triplets.append([nums[i]] + pair)
-
-    return triplets
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return triplets 
 
 def pair_sum_sorted_all_pairs(nums: List[int], start: int, target: int) -> List[int]:
     pairs = []
-    left, right = start, len(nums) - 1 
+    left, right = start, len(nums) -1
 
     while left < right:
         sum = nums[left] + nums[right]
+
         if sum == target:
-            # stuff
             pairs.append([nums[left], nums[right]])
             left += 1
-            # to avoid duplicate [b, c] pairs, skip b if its the same as the previous
-            while left < right and nums[left] == nums[left - 1]:
+            if left < right and nums[left] == nums[left - 1]:
                 left += 1
         elif sum < target:
             left += 1
         else:
             right -= 1
-
     return pairs
+
 
